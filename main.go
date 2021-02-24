dc4de6e431a60779467284cd06ba8f7732157f3f
kodejuice <kodejuice@gmail.com>
2021-02-24 03:52:12 +0100

    Compromiso inicial

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2020 Sochima Biereagu <sochima.agu1@gmail.com>
+This file is part of the CLI application googoo.
+*/
+package main
+
+import "github.com/kodejuice/googoo/cmd"
+
+func main() {
+	cmd.Execute()
+}
